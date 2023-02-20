a82665a20bd9f1575f2c497c28ba36942a08d3cb
nuriiiks <124987275+diasarziyev@users.noreply.github.com>
2023-02-20 19:28:18 +0600

    Add files via upload

---
+++ TSIS 4/generators/4.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = b*b
+for i in range (a,c):
+    if a*a<c:
+        print(a*a)
+    a=a+1
---
+++ TSIS 4/generators/5.py
@@ -0,0 +1,6 @@
+n = int(input())
+result = 0
+my_list =[]
+for i in range(0,n):
+    my_list.append(i)
+print(my_list[::-1])
